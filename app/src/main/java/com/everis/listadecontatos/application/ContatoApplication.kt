000e7e4a1a48c16ea0cde1a10182575393b1c9c7
Josebias Neto <josebiasbsneto@gmail.com>
2021-07-22 19:18:00 -0300

    Create HelperDB

---
+++ app/src/main/java/com/everis/listadecontatos/application/ContatoApplication.kt
@@ -0,0 +1,10 @@
+package com.everis.listadecontatos.application
+
+import android.app.Application
+
+class ContatoApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
+}
---
+++ app/src/main/java/com/everis/listadecontatos/helpers/HelperDB.kt
@@ -0,0 +1,42 @@
+package com.everis.listadecontatos.helpers
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class HelperDB(
+    context: Context?
+) : SQLiteOpenHelper(context, NOME_BANCO, null, VERSAO_ATUAL) {
+
+    companion object {
+        private val NOME_BANCO = "contato.db"
+        private val VERSAO_ATUAL = 1
+    }
+
+    val TABLE_NAME = "contato"
+    val COLUMNS_ID = "id"
+    val COLUMNS_NOME = "nome"
+    val COLUMNS_TELEFONE = "telefone"
+    val DROP_TABLE = "DROP TABLE IF EXISTS $TABLE_NAME"
+    val CREATE_TALBE = "CREATE TABLE $TABLE_NAME (" +
+            "$COLUMNS_ID INTEGER NOT NULL," +
+            "$COLUMNS_NOME TEXT NOT NULL," +
+            "$COLUMNS_TELEFONE TEXT NOT NULL," +
+            "" +
+            "PRIMARY KEY ($COLUMNS_ID AUTOINCREMENT)" +
+            ")"
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(CREATE_TALBE)
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        if(oldVersion != newVersion){
+            db?.execSQL(DROP_TABLE)
+            //update na tabela ou criar nova tabela
+        }
+        onCreate(db)
+        TODO("Not yet implemented")
+    }
+}
